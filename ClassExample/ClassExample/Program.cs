a18d212a8a5060da6200fc2b8c31ed076f445c76
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-02-04 23:54:38 +0400

    first step

---
+++ ClassExample/ClassExample/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int c;
+
+
+
+
+
+        }
+    }
+}
+
---
+++ ClassExample/ClassExample/Triangle.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassExample
+{
+    class Triangle
+    {
+       
+
+        public double Area(int a,int b,int b)
+        {
+           return  double P=
+
+
+        }
+
+
+
+    }
+}
