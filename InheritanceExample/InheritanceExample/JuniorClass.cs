cc2272233d6543d4e714ae887c63e3557ca8154a
nalbandyan.1991 <nalbandyan.1991@bk.ru>
2017-02-07 17:28:19 +0400

    error

---
+++ InheritanceExample/InheritanceExample/JuniorClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceExample
+{
+   class JuniorClass : BaseClass
+    {
+        public bool hybrid;
+        public bool electromobile;
+        public JuniorClass(bool hybrid,bool electromobile):
+            base(string  carname, int petrol, string country, int volumeEngine)
+        {
+            Console.WriteLine("b");
+            this.hybrid = hybrid;
+            this.electromobile = electromobile;
+        }
+    }
+
+
+
+
+
+}
